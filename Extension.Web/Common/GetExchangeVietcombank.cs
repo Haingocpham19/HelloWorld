2eb01ffd4b8ce7db8cd1455914ae29f55f0352b6
haipn-runsys <haipn@runsystem.net>
2023-08-14 17:55:27 +0700

    hai up

--- Extension.Web/Common/GetExchangeVietcombank.cs
+++ Extension.Web/Common/GetExchangeVietcombank.cs
@@ -8,35 +8,33 @@ using System.Threading.Tasks;
 
 namespace PCS.Extension.Common
 {
-    class GetExchangeVietcombank
+    public static class GetExchangeVietcombank
     {
-        public async Task<List<Currency>> GetExchange()
+        public static async Task<List<Currency>> GetExchange()
         {
-            List<Currency> listResult = new List<Currency>();
-
+            List<Currency> listResult = new();
             var options = new LaunchOptions
             {
-                Headless = false
+                Headless = false,
             };
             //Console.WriteLine("Downloading chromium");
             //Download the Chromium revision if it does not already exist
-            //await new BrowserFetcher().DownloadAsync();
+            await new BrowserFetcher().DownloadAsync();
 
             using (var browser = await Puppeteer.LaunchAsync(options))
+
             using (var page = await browser.NewPageAsync())
             {
                 string getDate = DateTime.Now.ToString("dd/MM/yy");
 
-                string url = $"https://portal.vietcombank.com.vn/UserControls/TVPortal.TyGia/pListTyGia.aspx?txttungay=" + getDate + "&BacrhID=1&isEn=True";
-
-                //string url = "https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx";
+                string url = "https://portal.vietcombank.com.vn/UserControls/TVPortal.TyGia/pListTyGia.aspx?txttungay=" + getDate + "&BacrhID=1&isEn=True";
 
                 await page.GoToAsync(url);
                 await page.WaitForTimeoutAsync(100);
                 int countCurrency = await page.EvaluateExpressionAsync<int>("document.querySelectorAll('.odd').length");
                 for (int i = 0; i < countCurrency; i++)
                 {
-                    Currency temp = new Currency()
+                    Currency temp = new()
                     {
                         CurencyId = i + 1,
                         CurrencyName = await page.EvaluateExpressionAsync<string>($"document.querySelector(\"#ctl00_Content_ExrateView > tbody > tr:nth-child({i + 3}) > td:nth-child(1)\").innerText"),
@@ -45,19 +43,22 @@ namespace PCS.Extension.Common
                     };
                     listResult.Add(temp);
                 }
-                // Folder, where a file is created.  
-                // Make sure to change this folder to your own folder  
-                string folder = Directory.GetCurrentDirectory();
-                // Filename  
-                string fileName = "SaveJsonFile\\InfoCurrencyEveryDay.json";
-                // Fullpath. You can direct hardcode it if you like. 
-                // Read a file  
-                string fullPath = Path.Combine(folder, fileName);
-                // write JSON directly to a file
-                string json = JsonConvert.SerializeObject(listResult);
-                System.IO.File.WriteAllText(fullPath, json);
-                Console.WriteLine("Completed");
+                await page.CloseAsync();
             }
+
+            // Folder, where a file is created.  
+            // Make sure to change this folder to your own folder  
+            string folder = Directory.GetCurrentDirectory();
+            // Filename  
+            const string fileName = "SaveJsonFile\\InfoCurrencyEveryDay.json";
+            // Fullpath. You can direct hardcode it if you like. 
+            // Read a file  
+            string fullPath = Path.Combine(folder, fileName);
+            // write JSON directly to a file
+            string json = JsonConvert.SerializeObject(listResult);
+            System.IO.File.WriteAllText(fullPath, json);
+            Console.WriteLine("Completed");
+
             return listResult;
         }
     }
--- Extension.Web/Controllers/GetExchangeRateController.cs
+++ Extension.Web/Controllers/GetExchangeRateController.cs
@@ -1,54 +1,48 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using PCS.Extension.Common;
-//using Extension.Domain.EF;
-//using Extension.Domain.Entities;
-//using Extension.Domain.Repositories;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+﻿using Microsoft.AspNetCore.Mvc;
+using PCS.Extension.Common;
+using Extension.Domain.Entities;
+using Extension.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Extension.Infracstructure;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-//namespace PCS.Extension.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class GetExchangeRateController : ControllerBase
-//    {
-//        // GET: api/<GetExchangeRateController>
-//        private readonly IResponseDataRepository _reponseDataRepository;
-//        private readonly ExtensionDbContext _ExtensionDbContext;
-//        public GetExchangeRateController(IResponseDataRepository reponseDataRepository, ExtensionDbContext ExtensionDbContext)
-//        {
-//            _reponseDataRepository = reponseDataRepository;
-//            _ExtensionDbContext = ExtensionDbContext;
-//        }
-//        [HttpGet]
-//        public async Task<List<Currency>> Get()
-//        {
-//            GetExchangeVietcombank temp = new GetExchangeVietcombank();
-//            var result = temp.GetExchange().Result;
-//            return result;
-//        }
-//        [HttpPost]
-//        public async Task<List<Currency>> Post()
-//        {
-//            DeserializeJsonCurrency deserialize = new DeserializeJsonCurrency();
-//            var outPut = deserialize.DeserializeJson().Result;
-//            foreach (var item in outPut)
-//            {
-//                Currency currency = new Currency()
-//                {
-//                    CurrencyCode = item.CurrencyCode,
-//                    CurrencyName = item.CurrencyName,
-//                    ExchangeRate = item.ExchangeRate,
-//                    GetDateTime = item.GetDateTime,
-//                    CurencyId = item.CurencyId
-//                };
-//                _reponseDataRepository.InsertCurrency(currency);
-//            }
-//            _reponseDataRepository.Save();
-//            return outPut;
-//        }
-//    }
-//}
+namespace PCS.Extension.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetExchangeRateController : ControllerBase
+    {
+        // GET: api/<GetExchangeRateController>
+        private readonly ExtensionDbContext extensionDbContext;
+        public GetExchangeRateController(ExtensionDbContext ExtensionDbContext)
+        {
+            extensionDbContext = ExtensionDbContext;
+        }
+        [HttpGet]
+        public async Task<List<Currency>> Get()
+        {
+            return await GetExchangeVietcombank.GetExchange();
+        }
+        [HttpPost]
+        public async Task<List<Currency>> Post()
+        {
+            DeserializeJsonCurrency deserialize = new();
+            var outPut = await deserialize.DeserializeJson();
+            foreach (var item in outPut)
+            {
+                Currency currency = new()
+                {
+                    CurrencyCode = item.CurrencyCode,
+                    CurrencyName = item.CurrencyName,
+                    ExchangeRate = item.ExchangeRate,
+                    GetDateTime = item.GetDateTime,
+                    CurencyId = item.CurencyId
+                };
+            }
+            return outPut;
+        }
+    }
+}
